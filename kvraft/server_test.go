2c501b5057d24040b25eedc76a7e704889904a46
李攀 <lipan.2000@bytedance.com>
2022-05-17 18:25:14 +0800

    basic kv

--- kvraft/server_test.go
+++ kvraft/server_test.go
@@ -3,6 +3,8 @@ package kvraft
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestServer(t *testing.T) {
@@ -13,52 +15,15 @@ func TestServer(t *testing.T) {
 	ck := cfg.makeClient(cfg.All())
 	time.Sleep(time.Second * 3)
 
-	// req := PutAppendArgs{
-	// 	Key:   "stuff",
-	// 	Value: "lee",
-	// 	Op:    "Put",
-	// }
-	// resp := PutAppendReply{}
 	ck.Put("stuff", "lee")
 	val := ck.Get("stuff")
-	D("value:%v", val)
+	assert.Equal(t, val, "lee")
 
-	ck.Put("", "")
-	val = ck.Get("")
-	D("value:%v", val)
-	// ck.servers[0].Call("KVServer.PutAppend", &req, &resp)
-	// if resp.Err == "" {
-	// 	D("[%v] success", 0)
-	// } else {
-	// 	D("[%d] error:%v", 0, resp.Err)
-	// }
-	// time.Sleep(time.Millisecond * 100)
+	ck.Put("name", "jack")
+	val = ck.Get("name")
+	assert.Equal(t, val, "jack")
 
-	// getReq := GetArgs{
-	// 	Key: "stuff",
-	// }
-	// getResp := GetReply{}
-	// ck.servers[0].Call("KVServer.Get", &getReq, &getResp)
-	// if resp.Err == "" {
-	// 	D("[%v] success,value:%v", 0, getResp.Value)
-	// } else {
-	// 	D("[%d] error:%v", 0, resp.Err)
-	// }
-
-	// ck.servers[1].Call("KVServer.PutAppend", &req, &resp)
-	// if resp.Err == "" {
-	// 	D("[%v] success", 1)
-	// } else {
-	// 	D("[%d] error:%v", 1, resp.Err)
-	// }
-	// time.Sleep(time.Millisecond * 100)
-
-	// ck.servers[2].Call("KVServer.PutAppend", &req, &resp)
-	// if resp.Err == "" {
-	// 	D("[%v] success", 2)
-	// } else {
-	// 	D("[%d] error:%v", 2, resp.Err)
-	// }
-
-	// ck.Put("stuff", "Lee")
+	ck.Append("stuff", "_haha")
+	val = ck.Get("stuff")
+	assert.Equal(t, val, "lee_haha")
 }
